45a225755bc35be0ad29105ac1cf831be3ceb2ec
Caio Barilli <caiobarilli@gmail.com>
2022-09-17 21:32:37 -0300

    Update _permissions.sh

--- _permissions.sh
+++ _permissions.sh
@@ -18,11 +18,11 @@ echo " "
 
 # docker exec -it $container_id /bin/bash
 
-# docker exec -it $container_id /bin/bash -c "
-# chown -R 1000:1000 /var/www/html/wp-content/themes
-# cd /var/www/html/wp-content
-# ls -lah
-# "
+docker exec -it $container_id /bin/bash -c "
+chown -R 1000:1000 /var/www/html/wp-content/themes
+cd /var/www/html/wp-content
+ls -lah
+"
 
 docker exec -it $container_id /bin/bash -c "
 cd /var/www/html/wp-content/themes
